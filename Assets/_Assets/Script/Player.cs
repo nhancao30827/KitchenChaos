204a574b6e5429f4bf368620da4477aefbef6a48
nhancao30827 <nhancao03ct@gmail.com>
2025-02-25 21:50:38 +0700

    Update learn folder

--- Assets/_Assets/Script/Player.cs
+++ Assets/_Assets/Script/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
         Vector3 playerInput = new Vector3(Input.GetAxis("Horizontal"), 0,  Input.GetAxis("Vertical")).normalized;
 
         Debug.Log(playerInput);
-
+        Debug.Log(Time.deltaTime);
         Vector3 movement = playerInput * _moveSpeed *Time.deltaTime;
         this.transform.position += movement;
     }
