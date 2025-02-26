d0777c4de73871a4f12de8d56fc4a7c1b493eddd
nhancao30827 <nhancao03ct@gmail.com>
2025-02-26 21:05:09 +0700

    Added to learn

--- Assets/_Assets/Script/Player.cs
+++ Assets/_Assets/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private float _moveSpeed = 5f;
+    private float _rotationSpeed = 20f;
 
     private void Update()
     {
@@ -12,7 +13,11 @@ public class Player : MonoBehaviour
 
         Debug.Log(playerInput);
         Debug.Log(Time.deltaTime);
+
         Vector3 movement = playerInput * _moveSpeed *Time.deltaTime;
+
         this.transform.position += movement;
+
+        this.transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime * _rotationSpeed);
     }
 }
